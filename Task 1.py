1b3612b9bd0ea344d0053ed0c5d8d3013741142a
swayam5123 <swayamshelar12@gmail.com>
2025-07-12 11:18:43 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,14 @@
+a=input("Enter a number: ")
+b=input("Enter another number: ")
+a=int(a)
+b=int(b)
+
+addition=a+b
+Subtraction=a-b
+multiplication=a*b
+Division=a/b
+
+print("addition is: ", addition)
+print("Subtraction is: ", Subtraction)
+print("multiplication is: ", multiplication)
+print("Division is: ", Division)
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+First_Name=input("Enter First Name: ")
+Last_Name=input("Enter Last Name: ")
+
+Full_name=(First_Name+" "+Last_Name)
+
+print('Hello,', Full_name, 'Wellcome to python program')
